36b4946e68553988ac26c25204eccfb0e707c6ec
Nicolas Bezerra <nicolasbezerra13@gmail.com>
2025-08-10 10:57:47 +0100

    feat: Add TrendingCard component, enhance movie details view, and update profile and saved screens with improved layouts and styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
+import TrendingCard from "@/components/TrendingCard";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { getMovies } from "@/services/api";
@@ -75,7 +76,7 @@ export default function Index() {
                 className="mb-4 mt-3"
                 data={trendingMovies}
                 renderItem={({ item, index }) => (
-                  <Text className="text-white text-sm">{item.title}</Text>
+                  <TrendingCard movie={item} index={index} />
                 )}
                 keyExtractor={(item) => item.movie_id.toString()}
               />
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,17 @@
+import { icons } from "@/constants/icons";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 const Saved = () => {
   return (
-    <View>
-      <Text>saved</Text>
+    <View className="flex-1 bg-primary px-10">
+      <View className="flex items-center justify-center flex-1 felx-col gap-5">
+        <Image source={icons.save} className="size-10" tintColor="#fff" />
+        <Text className="text-light-100 text-2xl font-semibold">Saved</Text>
+        <Text className="text-light-200 text-sm">
+          Manage your saved movies, TV shows, and more.
+        </Text>
+      </View>
     </View>
   );
 };
